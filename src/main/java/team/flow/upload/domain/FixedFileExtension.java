eefe90695874c91a273c5953f146a40486034330
Haebin <joker7011@naver.com>
2024-02-27 19:18:39 +0900

    feat: 고정 확장자 엔티티

---
+++ src/main/java/team/flow/upload/domain/FixedFileExtension.java
@@ -0,0 +1,29 @@
+package team.flow.upload.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.flow.upload.domain.constants.FileExtensionType;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Table(name = "t_fixed_file_extension")
+@NoArgsConstructor(access = PROTECTED)
+public class FixedFileExtension {
+
+    @Id
+    @Column(name = "fixed_file_extension_id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "file_extention_type")
+    private FileExtensionType fileExtensionType;
+
+    @Column(name = "is_restricted")
+    private boolean isRestricted;
+}
---
+++ src/main/java/team/flow/upload/domain/constants/FileExtensionType.java
@@ -0,0 +1,11 @@
+package team.flow.upload.domain.constants;
+
+public enum FileExtensionType {
+    BAT,
+    CMD,
+    COM,
+    CPL,
+    EXE,
+    SCR,
+    JS
+}
